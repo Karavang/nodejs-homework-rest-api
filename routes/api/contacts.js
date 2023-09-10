1ae2f50d64d5880fab34dcadc2a2f65050ffcd2a
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-10 22:11:00 +0200

    validate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const functions = require("../../models/contacts.js");
+const Joi = require("joi");
 const router = express.Router();
+const createError = (ERROR_TYPE, { message, data }) => {
+  return { type: ERROR_TYPE, message, data };
+};
+const ERROR_TYPES = {
+  BAD_REQUEST: "BAD_REQUEST",
+  UNAUTHORIZED: "UNAUTHORIZED",
+  FORBIDDEN: "FORBIDDEN",
+  NOT_FOUND: "NOT_FOUND",
+  INTERNAL_SERVER_ERROR: "INTERNAL_SERVER_ERROR",
+};
 
+const standartBody = Joi.object({
+  id: Joi.string(),
+  name: Joi.string().alphanum().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.number().required(),
+});
+const validate = (target) => (schema) => (req, res, next) => {
+  const result = schema.validate(req[target]);
+  if (result.error) {
+    const custorError = createError(ERROR_TYPES.BAD_REQUEST, {
+      data: result.error,
+      message: result.error.message,
+    });
+    next(custorError);
+  } else {
+    next();
+  }
+};
+const validateBody = validate("body");
 router.get("/", async (req, res, next) => {
   try {
     const contacts = await functions.listContacts();
@@ -25,7 +60,7 @@ router.get("/:contactId", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", [validateBody(standartBody)], async (req, res, next) => {
   console.log(req.body);
   if (req.body.name && req.body.email && req.body.phone) {
     await functions.addContact(req.body);
@@ -47,19 +82,23 @@ router.delete("/:contactId", async (req, res, next) => {
   }
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    if (req.body.name && req.body.email && req.body.phone) {
-      await functions.changeContact(req.params.contactId, req.body);
-      res.status(201);
-    } else {
-      res.json({ message: "missing fields" });
-      res.status(400);
+router.put(
+  "/:contactId",
+  [validateBody(standartBody)],
+  async (req, res, next) => {
+    try {
+      if (req.body.name && req.body.email && req.body.phone) {
+        await functions.changeContact(req.params.contactId, req.body);
+        res.status(201);
+      } else {
+        res.json({ message: "missing fields" });
+        res.status(400);
+      }
+    } catch (e) {
+      res.json({ message: "Not found" });
+      res.status(404);
     }
-  } catch (e) {
-    res.json({ message: "Not found" });
-    res.status(404);
   }
-});
+);
 
 module.exports = router;
