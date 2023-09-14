2113da8c3f901b41eaa8b1da8627bf700a14992b
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-14 11:43:11 +0200

    fixed

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -97,9 +97,26 @@ router.put(
   async (req, res, next) => {
     try {
       if (req.body.name || req.body.email || req.body.phone) {
-        const newContact = { id: req.params.contactId, ...req.body };
-        await functions.changeContact(req.params.contactId, req.body);
-        res.status(200).json(newContact);
+        if (req.body.name && req.body.email && req.body.phone) {
+          const newContact = { id: req.params.contactId, ...req.body };
+          await functions.changeContact(req.params.contactId, req.body);
+          res.status(200).json(newContact);
+        } else {
+          res.status(400);
+          const missingFields = [];
+          if (!req.body.name) {
+            missingFields.push("name");
+          }
+          if (!req.body.email) {
+            missingFields.push("email");
+          }
+          if (!req.body.phone) {
+            missingFields.push("phone");
+          }
+          res.json({
+            message: `missing required ${missingFields.join(", ")} field`,
+          });
+        }
       } else {
         res.status(400).json({ message: "Missing fields" });
         return;
