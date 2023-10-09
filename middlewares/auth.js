6f65c557d15781568f425d3424289b76759fc44c
Karavang <k4r4v4ng@gmail.com>
2023-10-09 20:11:55 +0200

    lst

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,23 +4,21 @@ const { Users } = require("../forDb");
 
 const auth = async (req, res, next) => {
   try {
-    if (req.headers.authorization !== undefined) {
-      const { authorization = "" } = req.headers;
-      const [bearer, token] = authorization.split(" ");
-      console.log(bearer);
-      console.log(token);
-      if (bearer === "Bearer") {
-        const { id } = jwt.verify(token, JWTSECRET);
+    const { authorization = "" } = req.headers;
+    const [bearer, token] = authorization.split(" ");
+    console.log(bearer);
+    console.log(token);
+    if (bearer === "Bearer") {
+      const { id } = jwt.verify(token, JWTSECRET);
 
-        const user = await Users.findById(id);
-        if (!user || user.token !== token) {
-          res.status(401).json({ message: "Not authorized" });
-          return;
-        }
-
-        req.user = user;
-        next();
+      const user = await Users.findById(id);
+      if (!user || user.token !== token) {
+        res.status(401).json({ message: "Not authorized" });
+        return;
       }
+
+      req.user = user;
+      next();
     } else {
       res.status(401).json({ message: "Not authorized" });
     }
